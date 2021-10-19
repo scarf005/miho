36f924d43017cf14b5f2424c17648cdae7f14306
scarf <greenscarf005@gmail.com>
2021-10-19 20:40:12 +0900

    build: :construction_worker: use github pages deploy for CI

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,20 @@
-const path = require(`path`)
-const HtmlWebpackPlugin = require(`html-webpack-plugin`)
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  mode: `none`,
-  mode: `development`,
-  entry: `./src/index.js`,
+  mode: 'development',
+  entry: './src/index.js',
   output: {
-    filename: `main.js`,
-    path: path.resolve(__dirname, `dist`),
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
   },
   module: {
-    rules: [{ test: /\.pug$/, use: 'pug-loader' }],
+    rules: [{ test: /\.pug$/, use: 'pug-loader' }]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      filename: `index.html`,
-      template: `src/index.pug`,
-    }),
-  ],
+      filename: 'index.html',
+      template: 'src/index.pug'
+    })
+  ]
 }
