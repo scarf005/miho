35980f41cb200ef753eb1f3fa3b037a780934451
scarf <greenscarf005@gmail.com>
2021-10-19 18:42:08 +0900

    first dist

---
+++ main.js
@@ -0,0 +1,8 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+const canvas = document.getElementById('canvas')
+const context = canvas.getContext('2d')
+context.fillRect(0, 0, 20, 20, 20)
+
+/******/ })()
+;
