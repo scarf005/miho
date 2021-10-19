5ade94119c7222c8b241dda70cafd856564e5b11
pig005 <greenscarf005@gmail.com>
2021-10-19 12:09:48 +0000

    Deploying to gh-pages from @ pig005/miho@6af1affa7b52fbc283c5ad11caa15e270ea7ab50 🚀

--- README.md
+++
@@ -1,2 +0,0 @@
-# miho
-Second broughlike attempt after 1.5 years. Control Miho to win
--- src/index.js
+++
@@ -1,3 +0,0 @@
-const canvas = document.getElementById('canvas')
-const context = canvas.getContext('2d')
-context.fillRect(0, 0, 20, 20, 20)
