99c707ea22ea3842ca4bfca6e43c366219c82b46
scarf <greenscarf005@gmail.com>
2021-10-19 12:50:41 +0900

    build: :construction_worker: update npm build

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+// modern js not supported
+// use of require is mandatory
+// please don't try to refactor it into import
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+}
